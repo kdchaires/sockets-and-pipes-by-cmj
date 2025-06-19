98314fe819fbf40447e4ba741fac9241536a3054
Karina Chaires <kdchaires@gmail.com>
2025-06-18 23:45:31 -0600

    chap1-add example of file handle

--- book/Book.hs
+++ book/Book.hs
@@ -1,13 +1,23 @@
-module Book  where
+module Book where
 
-import Prelude ()
-import Relude
+import           Prelude ()
+import           Relude
 
 import qualified System.Directory as Dir
-import System.FilePath((</>))
+import           System.FilePath ((</>))
+
+import qualified System.IO as IO
 
 getDataDir :: IO FilePath
 getDataDir = do
     dir <- Dir.getXdgDirectory Dir.XdgData "sockets-and-pipes"
     Dir.createDirectoryIfMissing True dir
     return dir
+
+writeGreetingFile :: IO ()
+writeGreetingFile = do
+    dir <- getDataDir
+    h   <- IO.openFile (dir </> "greeting.txt") WriteMode
+    IO.hPutStrLn h "hello"
+    IO.hPutStrLn h "word"
+    IO.hClose h
